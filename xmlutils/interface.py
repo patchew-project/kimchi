0afbf68696c5fe149064f1e39683f1b13c080cab
Archana Singh <archus@linux.vnet.ibm.com>
2016-08-31 03:14:27 +0530

    Updated code to support 'interfaces' parameter to template API only on s390x/s390 architecture.
    1) Added i18n error codes.
    2) Added interface as new kimchitype in API.json. And ref added in update template.
         -Interface expects an object with parameters: 'name', 'type' and 'mode'.
         -Name should be name of host network interface(Ethernet, Bond, VLAN) for type 'macvtap'
          or name should be name of host openvswitch bridge interface for type ovs.
         -Mode only applicable for interface type macvtap to indicates whether packets
          has to be delivered directly to target device(bridge) or to the external bridge(vepa-capable bridge).
         -Mode is Optional.
    3) Added interfaces as optional attribute in template control.
         -This attribute returns to be list of host interface attached to template,
          each interface will have above attribute.
         -Empty list if no host network interface is attached to template.
    4) Added method to generate valid interface xml from the interface based on above attribute.
         -This will use below method to generate interface xml based on type and other attribute.
         -Update vm xml generate method to add generated interfaces xml to vm xml.
    5) Modified xmlutils which generates interface xml based on interface type and attributes.
    6) Added validate_interfaces method to validate that interfaces are only
    supported on s390x/s390 architecture.
    
    Signed-off-by: Archana Singh <archus@linux.vnet.ibm.com>
    Message-Id: <1472593468-16628-2-git-send-email-archus@linux.vnet.ibm.com>

--- xmlutils/interface.py
+++ xmlutils/interface.py
@@ -24,6 +24,16 @@ from wok.plugins.kimchi import osinfo
 
 
 def get_iface_xml(params, arch=None, os_distro=None, os_version=None):
+    typ = params.get('type', 'network')
+    if typ == 'network':
+        return get_iface_network_xml(params, arch, os_distro, os_version)
+    elif typ == 'bridge':
+        return get_iface_ovs_xml(params, arch, os_distro, os_version)
+    elif typ == 'direct':
+        return get_iface_macvtap_xml(params, arch, os_distro, os_version)
+
+
+def get_iface_network_xml(params, arch=None, os_distro=None, os_version=None):
     """
     <interface type='network' name='ethX'>
       <start mode='onboot'/>
@@ -60,5 +70,58 @@ def get_iface_xml(params, arch=None, os_distro=None, os_version=None):
     mac = params.get('mac', None)
     if mac is not None:
         interface.append(E.mac(address=mac))
+
+    return ET.tostring(interface, encoding='utf-8', pretty_print=True)
+
+
+def get_iface_macvtap_xml(params, arch=None, os_distro=None, os_version=None):
+    """
+    <interface type="direct">
+      <source dev="bondX" mode="bridge"/>
+      <model type="virtio"/>
+    </interface>
+    """
+    device = params['name']
+    interface = E.interface(type=params['type'])
+    mode = params.get('mode', None)
+    if mode is not None:
+        interface.append(E.source(dev=device, mode=mode))
+    else:
+        interface.append(E.source(dev=device))
+
+    model = params.get('model', None)
+
+    # only append 'model' to the XML if it's been specified as a parameter
+    # otherwise let libvirt use its default value
+    if model is not None:
+        interface.append(E.model(type=model))
+
+    interface.append(E.model(type=model))
+
+    return ET.tostring(interface, encoding='utf-8', pretty_print=True)
+
+
+def get_iface_ovs_xml(params, arch=None, os_distro=None, os_version=None):
+    """
+    <interface type="bridge">
+      <source bridge="vswitchX"/>
+      <virtualport type="openvswitch"/>
+      <model type="virtio"/>
+    </interface>
+    """
+    device = params['name']
+    interface = E.interface(type=params['type'])
+    interface.append(E.source(bridge=device))
+    virtualport_type = params.get('virtualport_type', 'openvswitch')
+    interface.append(E.virtualport(type=virtualport_type))
+
+    model = params.get('model', None)
+
+    # only append 'model' to the XML if it's been specified as a parameter
+    # otherwise let libvirt use its default value
+    if model is not None:
+        interface.append(E.model(type=model))
+
+    interface.append(E.model(type=model))
 
     return ET.tostring(interface, encoding='utf-8', pretty_print=True)
