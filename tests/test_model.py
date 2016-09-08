1a8c33e67c0587920b2faf0c02348987f8df224d
Bianca Carvalho <bianca@linux.vnet.ibm.com>
2016-09-08 10:35:11 -0300

    Feature request (#860): Support Guest Autostart
    Include 'autostart' option in API.json and vms.py (lookup and update)
    using libvirt dom.setAutostart to set as true or false. Also edit
    test_model.py to include those changes and updated API.md file.
    Also included API tests to test_rest.py in test_edit_vm function.
    
    Signed-off-by: Bianca Carvalho <bianca@linux.vnet.ibm.com>
    Message-Id: <1473341711-98404-1-git-send-email-bianca@linux.vnet.ibm.com>

--- tests/test_model.py
+++ tests/test_model.py
@@ -129,7 +129,7 @@ class ModelTests(unittest.TestCase):
         keys = set(('name', 'state', 'stats', 'uuid', 'memory', 'cpu_info',
                     'screenshot', 'icon', 'graphics', 'users', 'groups',
                     'access', 'persistent', 'bootorder', 'bootmenu', 'title',
-                    'description'))
+                    'description', 'autostart'))
 
         stats_keys = set(('cpu_utilization', 'mem_utilization',
                           'net_throughput', 'net_throughput_peak',
@@ -1357,6 +1357,12 @@ class ModelTests(unittest.TestCase):
             self.assertEquals("yes", inst.vm_lookup(u'пeω-∨м')['bootmenu'])
             inst.vm_update(u'пeω-∨м', {"bootmenu": False})
             self.assertEquals("no", inst.vm_lookup(u'пeω-∨м')['bootmenu'])
+
+            # enable/disable autostart
+            inst.vm_update(u'пeω-∨м', {"autostart": True})
+            self.assertEquals(1, inst.vm_lookup(u'пeω-∨м')['autostart'])
+            inst.vm_update(u'пeω-∨м', {"autostart": False})
+            self.assertEquals(0, inst.vm_lookup(u'пeω-∨м')['autostart'])
 
     def test_get_interfaces(self):
         inst = model.Model('test:///default',
